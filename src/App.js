baa7bad87e3718fd559b8d105155c285d27e18f8
hchao <helenchao01@gmail.com>
2024-09-27 18:29:57 -0700

    Delete extra files

--- src/App.js
+++ src/App.js
@@ -6,60 +6,31 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navigation from './components/Navigation.js';
 import './App.css';
 
-// Import pages you have completed:
-// Home, Topics, Gallery, Contact, and Staff Pages 
-// import products from './data/products.js;'
+// Import completed pages
 import HomePage from './pages/HomePage.js';
-import TopicPage from './pages/TopicPage.js';
-//import GalleryPage from './pages/GalleryPage.js';
-import OrderPage from './pages/OrderPage.js';
 import FrontEndProjectsPage from './pages/FrontEndProjectsPage.js';
 import OtherProjectsPage from './pages/OtherProjectsPage.js';
 import LeadershipPage from './pages/LeadershipPage.js';
-//import ContactPage from './pages/ContactPage.js';
 
-// For Create and Edit, use the form OR table design; not both.
-// If your schema requires LONG data input, then use the FORM design:
-import AddCafePageForm from './pages/AddCafePageForm';
-import EditCafePageForm from './pages/EditCafePageForm';
-
-// // If your schema requires SHORT data input, then use the TABLE design.
-// import EditMoviePageTable from './pages/EditMoviePageTable';
-// import AddMoviePageTable from './pages/AddMoviePageTable';
-
-// Define the function that renders the content in Routes, using State.
+// Function that renders the content in Routes, using State.
 function App() {
 
   return (
       <BrowserRouter>
-
-          {/* <header>
-            <h1>Helen <img src="/android-chrome-192x192.png" /> Chao</h1>
-            <p>This is a portfolio of my interests, hobbies, coding projects, and cafe discoveries. Feel free to log your cafe visits too!</p>
-          </header> */}
 
           <Navigation />
 
           <main>
             <section>
                 <Routes> 
-                    {/* Add Routes for Home, Topics, Gallery, Contact, and Staff Pages.  */}
-                    
-                    {/* <Route path="/" element={<MoviesPage setMovie={setMovieToEdit}/>} /> */}
                     <Route path="/" element={<HomePage/>} />
                     <Route path="/pages/HomePage.js" element={<HomePage/>} />
-                    {/* <Route path="/pages/TopicPage.js" element={<TopicPage/>} />
-                    <Route path="/pages/OrderPage.js" element={<OrderPage/>} /> */}
                     <Route path="/pages/FrontEndProjectsPage.js" element={<FrontEndProjectsPage/>} />
                     <Route path="/pages/OtherProjectsPage.js" element={<OtherProjectsPage/>} />
                     <Route path="/pages/LeadershipPage.js" element={<LeadershipPage/>} />
                 </Routes>
               </section>
           </main>
-
-          {/* <footer>
-            <p>&copy; 2024 Helen Chao</p>
-          </footer> */}
 
       </BrowserRouter>
   );
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,22 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-// Change the function names and links
-// to fit your portfolio topic.
-
 function Navigation() {
   return (
     <nav>
-        {/* Add links to Home, Topics, Gallery, Contact, and Staff Pages  */}
-        {/* I haven't worked on any pages besides Home and Topics,
-        so I only added their links for now */}
         <Link to="/">Home</Link>
-        {/* <Link to="/pages/TopicPage.js">Topics</Link>
-        <Link to="/pages/OrderPage.js">Order</Link> */}
         <Link to="/pages/FrontEndProjectsPage.js">Front-End Projects</Link>
         <Link to="/pages/OtherProjectsPage.js">Chess Project</Link>
         <Link to="/pages/LeadershipPage.js">Leadership</Link>
-        {/* <Link to="/pages/HomePage.js">Home</Link> */}
     </nav>
   );
 }
