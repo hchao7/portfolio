4905fe159334b69a9df2e37e5b54719fc87949df
hchao <helenchao01@gmail.com>
2024-10-02 18:42:38 -0700

    Edit Home Page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -7,7 +7,7 @@ function HomePage() {
         <img class="profile__image" src={`${process.env.PUBLIC_URL}/temp-pic.png`} alt="Woman looking out window"/>
         <div class="profile__component">
           <h2 class="profile__heading">Hi, I'm Helen!</h2>
-          <p class="profile__description">I’m a Computer Science major at Oregon State University, set to graduate in Winter/Spring 2026, and I'm currently <span class="emphasis--black">seeking summer internships for 2025</span>. 
+          <p class="profile__description">I’m a Computer Science major at Oregon State University, and I'm currently <span class="emphasis--black">seeking summer internships for 2025</span>. 
           This is my second bachelor’s degree; I previously graduated from UCLA with a BS in Cognitive Science.
           Feel free to check out the projects I've completed so far! If you'd like to connect, you can find me on <a
             href="https://www.linkedin.com/in/helen-chao-01/"
